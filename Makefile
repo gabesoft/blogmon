b6487a7b26e7980784c1d8894ee5ccbc68a08020
gabriel <gabesoft@gmail.com>
2012-03-15 21:52:41 -0700

    unit tests shells

--- Makefile
+++ Makefile
@@ -4,8 +4,8 @@ REPORTER = dot
 SRC = $(shell find lib/*.js)
 
 test: 
-				@NODE_ENV=test ./node_modules/.bin/mocha \
-							--reporter $(REPORTER) \
-							$(TESTS)
+		@NODE_ENV=test ./node_modules/.bin/mocha \
+						--reporter $(REPORTER) \
+						$(TESTS)
 
 .PHONY: test
--- test/task.js
+++ test/task.js
@@ -1,11 +1,11 @@
-var task = require('../server/task.js');
+var task = require('../lib/task.js');
 
 describe('task.delay()', function() {
     it('should run a task delayed', function(done) {
-
+        done();
     });
 
     it('should run a task at interval', function(done) {
-
+        done();
     });
 });
