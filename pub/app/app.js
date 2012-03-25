4db95c6a2a25db81540b3c2e82746ef783a482e3
gabriel <gabesoft@gmail.com>
2012-03-25 11:12:49 -0700

    routes fixes

--- pub/app/app.js
+++ pub/app/app.js
@@ -7,17 +7,17 @@
     var JSON = require('./dep/json2.js');
 
     console.log('jQuery', $);
-    console.log('backbone', Backbone); // TODO: make br-backbone with latest version
+    console.log('backbone', Backbone);
     console.log('dateFormat', dateFormat);
     console.log('tmpl', tmpl);
     console.log('JSON', JSON);
     console.log('underscore', _);
-    _.each([1,2,3,4], function(x) { console.log('x', x); });
 
     $(document).ready(function() {
         console.log(JSON.stringify({ a: 1, b: 'two' }));
         console.log($('.container'));
         console.log((new Date()).format('ddd, mm/dd hh:MM:ss TT'));
         console.log(dateFormat(new Date(), 'ddd, mm/dd hh:MM:ss TT'));
+        _.each([1,2,3,4], function(x) { console.log('x', x); });
     });
 })(this);
