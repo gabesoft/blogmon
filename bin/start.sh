173144cd2cc1218f85cb381a66dfd79fcaec82f4
gabriel <gabesoft@gmail.com>
2012-03-24 21:57:57 -0700

    views + routes

--- bin/start.sh
+++ bin/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 dir=`dirname $0`
 
-NODE_ENV=development node $dir/../server.js
+#NODE_ENV=development node $dir/../server.js
+NODE_ENV=development PORT=3000 nodemon --debug $dir/../server.js
--- server.js
+++ server.js
@@ -12,4 +12,4 @@
         console.log("Express server listening on port %d in %s mode", 
           app.address().port, app.settings.env);
     });
-};)
+}).call(this);
