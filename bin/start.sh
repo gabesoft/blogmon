21e8e7b9c75fa927e55d78a8e54b510c65b0ad4a
gabriel <gabesoft@gmail.com>
2012-04-21 13:34:31 -0700

    added base stylesheet

--- bin/start.sh
+++ bin/start.sh
@@ -2,4 +2,7 @@
 dir=`dirname $0`
 
 #NODE_ENV=development node $dir/../server.js
-NODE_ENV=development PORT=3000 nodemon --debug $dir/../server.js
+NODE_ENV=development PORT=3000 nodemon \
+  --debug $dir/../server.js \
+  --watch $dir/../lib \
+  --watch $dir/../pub \
