af81a9d350c904cb8c58ed600313c567fc5a439e
gabriel <gabesoft@gmail.com>
2012-04-21 21:40:56 -0700

    fonts + css

--- bin/stylus.sh
+++ bin/stylus.sh
@@ -2,11 +2,10 @@
 dir=`dirname $0`
 
 # TODO: use the --compress option for production
+#       remove  --firebug & --line-numbers
 
 $dir/../node_modules/stylus/bin/stylus \
   --use nib \
   --out $dir/../pub/css \
   --watch $dir/../pub/stylesheets \
-  --firebug \
   --line-numbers
-
