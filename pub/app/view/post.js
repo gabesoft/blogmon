e9b1239bd9afe5f43c2cd43360e43fc69a1a1069
gabriel <gabesoft@gmail.com>
2012-04-23 13:55:59 -0700

    posts styling - in progress

--- pub/app/view/post.js
+++ pub/app/view/post.js
@@ -6,8 +6,13 @@ var backbone = require('../dep/backbone.js')
 module.exports = backbone.View.extend({
     tagName: 'li',
 
+    events: {
+        'click .header' : 'toggleDescription'
+    },
+
     initialize: function(config) {
         this.template = mustache.compile($('#post-template').html());
+        _.bindAll(this, 'toggleDescription');
     },
 
     render: function() {
@@ -16,6 +21,18 @@ module.exports = backbone.View.extend({
 
         this.$el.html(html);
         return this;
+    },
+
+    toggleDescription: function() {
+        var desc      = this.$el.find('.description')
+          , cls       = 'collapsed'
+          , collapsed = desc.hasClass(cls);
+
+        if (collapsed) {
+            desc.removeClass(cls);
+        } else {
+            desc.addClass(cls);
+        }
     }
 });
 
