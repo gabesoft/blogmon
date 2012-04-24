67b62b955f94e3184ce956844a3eba9892fa37e1
gabriel <gabesoft@gmail.com>
2012-04-23 20:41:44 -0700

    posts styling 2

--- pub/app/view/post.js
+++ pub/app/view/post.js
@@ -1,6 +1,7 @@
 var backbone = require('../dep/backbone.js')
   , $        = require('jquery')
   , _        = require('../dep/underscore.js')
+  , datef    = require('../dep/date-format.js')
   , mustache = require('../dep/mustache.js');
 
 module.exports = backbone.View.extend({
@@ -17,13 +18,18 @@ module.exports = backbone.View.extend({
 
     render: function() {
         var post = this.model.toJSON()
-          , html = this.template(post);
+          , html = null;
+
+        post.dateStr = new Date(post.date).format('ddd, mm/dd/yyyy');
+        html         = this.template(post);
 
         this.$el.html(html);
         return this;
     },
 
-    toggleDescription: function() {
+    toggleDescription: function(e) {
+        if (e.target.nodeName === 'A') { return; }
+
         var content   = this.$el.find('.post-content')
           , ccls      = 'collapsed'
           , ecls      = 'expanded'
